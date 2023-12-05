32b64023793426aa63b56b186fb6cbac5ed509f6
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-12-05 00:03:19 -0300

    feat: added auth guard

--- src/auth/interfaces/auth.interfaces.ts
+++ src/auth/interfaces/auth.interfaces.ts
@@ -1,3 +1,10 @@
 export interface ITokenResponse {
     access_token: string
 }
+export interface ITokenUserRequest {
+    user: {
+        sub: string
+        iat: number
+        exp: number
+    }
+}
